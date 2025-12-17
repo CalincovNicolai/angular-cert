cd23979d68cc87204f477a8a934158fd33626a03
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2025-12-17 12:29:37 +0200

    feat: bind movie data to movie-item component and update template

--- create-a-component-driven-by-inputs-1/src/app/movie-item/movie-item.component.ts
+++ create-a-component-driven-by-inputs-1/src/app/movie-item/movie-item.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, input } from '@angular/core';
+import { Movie } from '../model/movie.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie-item',
   template: `
     <div class="movie-item">
       <div>
-        <h4>Test movie</h4>
+        <h4>{{ movie().title }}</h4>
         <small class="subtitle">
-          <span>Release date: 2024-01-02</span>
+          <span>Release date: {{ movie().release_date }}</span>
+          <span>Budget: {{ movie().budget | currency }} million</span>
+          <span>Duration: {{ movie().duration }} min</span>
         </small>
       </div>
       <button>Details</button>
     </div>
   `,
   standalone: true,
-  styleUrls: [ 'movie-item.component.scss' ]
+  styleUrls: ['movie-item.component.scss'],
+  imports: [CurrencyPipe],
 })
 export class MovieItemComponent {
-
+  movie = input.required<Movie>();
 }
-
