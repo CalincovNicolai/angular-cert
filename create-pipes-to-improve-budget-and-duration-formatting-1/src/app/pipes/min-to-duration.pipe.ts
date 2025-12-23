189399125acc74971fd320cd494d077ed2df7aac
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2025-12-23 11:54:29 +0200

    feat: implement millionDollar and minToDuration pipes for budget and duration formatting

--- create-pipes-to-improve-budget-and-duration-formatting-1/src/app/pipes/min-to-duration.pipe.ts
+++ create-pipes-to-improve-budget-and-duration-formatting-1/src/app/pipes/min-to-duration.pipe.ts
@@ -1,10 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'minToDuration'
+  name: 'minToDuration',
 })
 export class MinToDurationPipe implements PipeTransform {
-  transform(value: unknown): unknown {
-    return null;
+  transform(minutes?: string): string {
+    if (!minutes) return '';
+
+    const hours = Math.floor(+minutes / 60);
+    const min = +minutes % 60;
+    return `${hours}h ${min}min`;
   }
 }
