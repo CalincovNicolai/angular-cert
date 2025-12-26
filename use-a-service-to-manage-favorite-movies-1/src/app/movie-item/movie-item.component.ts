329745787b3b71ca1882e6f2a88fc53c3944347b
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2025-12-26 12:45:04 +0200

    feat: enhance movie item component with favorite functionality

--- use-a-service-to-manage-favorite-movies-1/src/app/movie-item/movie-item.component.ts
+++ use-a-service-to-manage-favorite-movies-1/src/app/movie-item/movie-item.component.ts
@@ -1,17 +1,24 @@
-import {Component, input} from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { Movie } from '../model/movie.model';
-import {MillionDollarPipe} from '../pipes/million-dollar.pipe';
-import {MinToDurationPipe} from '../pipes/min-to-duration.pipe';
+import { MillionDollarPipe } from '../pipes/million-dollar.pipe';
+import { MinToDurationPipe } from '../pipes/min-to-duration.pipe';
 
 @Component({
   selector: 'app-movie-item',
   template: `
     <div class="movie-item">
       <div>
-        <h4> <span class="icon-star"></span> {{ movie().title }}</h4>
+        <h4>
+          <span
+            class="icon-star"
+            [class.active]="isFavorite()"
+            (click)="toggleFavoriteChange(!isFavorite())"
+          ></span>
+          {{ movie().title }}
+        </h4>
         <small class="subtitle">
           <span>Release date: {{ movie().release_date }}</span>
-          <span>Budget:  {{ movie().budget | millionDollar }} </span>
+          <span>Budget: {{ movie().budget | millionDollar }} </span>
           <span>Duration: {{ movie().duration | minToDuration }}</span>
         </small>
       </div>
@@ -19,13 +26,15 @@ import {MinToDurationPipe} from '../pipes/min-to-duration.pipe';
       <button>Details</button>
     </div>
   `,
-  imports: [
-    MillionDollarPipe,
-    MinToDurationPipe
-  ],
-  styleUrls: ['movie-item.component.scss']
+  imports: [MillionDollarPipe, MinToDurationPipe],
+  styleUrls: ['movie-item.component.scss'],
 })
 export class MovieItemComponent {
-  movie  = input.required<Movie>();
-}
+  movie = input.required<Movie>();
+  isFavorite = input.required<boolean>();
+  toggleFavorite = output<boolean>();
 
+  toggleFavoriteChange(isFavorite: boolean): void {
+    this.toggleFavorite.emit(isFavorite);
+  }
+}
