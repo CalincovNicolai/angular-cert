5ef80a67b8f19d2b5d363fe6d1308f201d337992
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2026-01-12 10:50:03 +0200

    feat: update movie data handling to use async pipe for improved performance and readability

--- use-rx-js-observables-to-display-data-1/src/app/home/home.component.ts
+++ use-rx-js-observables-to-display-data-1/src/app/home/home.component.ts
@@ -1,20 +1,19 @@
-import {Component, inject, Signal} from '@angular/core';
-import {HighlightDirective} from '../highlight.directive';
-import {MovieItemComponent} from '../movie-item/movie-item.component';
-import {Movie} from '../model/movie.model';
-import {MoviesService} from '../services/movies.service';
-import {FavoritesService} from '../services/favorites.service';
+import { Component, inject } from '@angular/core';
+import { HighlightDirective } from '../highlight.directive';
+import { MovieItemComponent } from '../movie-item/movie-item.component';
+import { Movie } from '../model/movie.model';
+import { MoviesService } from '../services/movies.service';
+import { FavoritesService } from '../services/favorites.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [
-      HighlightDirective,
-      MovieItemComponent
-  ],
+  imports: [HighlightDirective, MovieItemComponent, AsyncPipe],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  protected movies: Signal<Movie[]> = inject(MoviesService).getMovies();
+  protected movies$: Observable<Movie[]> = inject(MoviesService).getMovies();
   protected favoritesService = inject(FavoritesService);
 }
