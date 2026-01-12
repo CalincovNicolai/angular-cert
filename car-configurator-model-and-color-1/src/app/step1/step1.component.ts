1b26dcfc3d83424569cd207d817f875ee895300a
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2026-01-12 12:06:54 +0200

    feat: initialize car configurator app with routing, components, services, and mock data

---
+++ car-configurator-model-and-color-1/src/app/step1/step1.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-step1',
+  imports: [
+      ReactiveFormsModule
+  ],
+  templateUrl: './step1.component.html',
+  styleUrl: './step1.component.scss'
+})
+export class Step1Component {
+
+}
