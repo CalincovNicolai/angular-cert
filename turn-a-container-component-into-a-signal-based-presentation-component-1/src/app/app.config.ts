bcdbee4b3002e948985766ae9531827a2bb2d560
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2026-01-29 15:36:20 +0200

    feat: refactor routing and movie details component for improved data handling

--- turn-a-container-component-into-a-signal-based-presentation-component-1/src/app/app.config.ts
+++ turn-a-container-component-into-a-signal-based-presentation-component-1/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {mockingInterceptor} from '../main';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { mockingInterceptor } from '../main';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptors([mockingInterceptor])),
-    provideRouter(routes)
-  ]
+    provideRouter(routes, withComponentInputBinding()),
+  ],
 };
