e92aa22fc7db7f37e59048e07b2f6871310c5051
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2026-02-09 15:42:58 +0200

    feat: implement favorite movie directive and update movie item component

--- create-a-directive-to-manage-favorite-movies-1/src/app/favorite.directive.ts
+++ create-a-directive-to-manage-favorite-movies-1/src/app/favorite.directive.ts
@@ -1,9 +1,27 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+} from '@angular/core';
+import { FavoritesService } from './services/favorites.service';
+import { Movie } from './model/movie.model';
 
 @Directive({
-  selector: '[appFavorite]'
+  selector: '[appFavorite]',
 })
 export class FavoriteDirective {
+  private favoritesService = inject(FavoritesService);
 
+  movie = input.required<Movie>({ alias: 'appFavorite' });
 
+  @HostBinding('class.active')
+  protected isFavorite = false;
+
+  @HostListener('click')
+  protected toggleFavorite(): void {
+    this.favoritesService.toggleFavorite(this.movie());
+    this.isFavorite = this.favoritesService.isFavorite(this.movie())();
+  }
 }
--- create-a-directive-to-manage-favorite-movies-1/src/app/movie-item/movie-item.component.ts
+++ create-a-directive-to-manage-favorite-movies-1/src/app/movie-item/movie-item.component.ts
@@ -1,8 +1,9 @@
-import {Component, input, output} from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { Movie } from '../model/movie.model';
-import {MillionDollarPipe} from '../pipes/million-dollar.pipe';
-import {MinToDurationPipe} from '../pipes/min-to-duration.pipe';
-import {RouterLink} from '@angular/router';
+import { MillionDollarPipe } from '../pipes/million-dollar.pipe';
+import { MinToDurationPipe } from '../pipes/min-to-duration.pipe';
+import { RouterLink } from '@angular/router';
+import { FavoriteDirective } from '../favorite.directive';
 
 @Component({
   selector: 'app-movie-item',
@@ -10,29 +11,27 @@ import {RouterLink} from '@angular/router';
     <div class="movie-item">
       <div>
         <h4>
-          <span class="icon-star" [class.active]="isFavorite()" (click)="toggleFavorite.emit(movie())"></span>
+          <span class="icon-star" [appFavorite]="movie()"></span>
           {{ movie().title }}
         </h4>
         <small class="subtitle">
           <span>Release date: {{ movie().release_date }}</span>
-          <span>Budget:  {{ movie().budget | millionDollar }} </span>
+          <span>Budget: {{ movie().budget | millionDollar }} </span>
           <span>Duration: {{ movie().duration | minToDuration }}</span>
         </small>
       </div>
 
-      <button [routerLink]="'details/'+ movie().id">Details</button>
+      <button [routerLink]="'details/' + movie().id">Details</button>
     </div>
   `,
   imports: [
     MillionDollarPipe,
     MinToDurationPipe,
-    RouterLink
+    RouterLink,
+    FavoriteDirective,
   ],
-  styleUrls: ['movie-item.component.scss']
+  styleUrls: ['movie-item.component.scss'],
 })
 export class MovieItemComponent {
-  movie  = input.required<Movie>();
-  isFavorite = input<boolean>(false);
-  toggleFavorite = output<Movie>();
+  movie = input.required<Movie>();
 }
-
