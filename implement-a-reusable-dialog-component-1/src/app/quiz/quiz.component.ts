b5fbf5e565e4ebf5dd3da3f9688c21901486499b
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2026-02-11 18:35:51 +0200

    feat: implement reusable dialog component for quiz submission confirmation

--- implement-a-reusable-dialog-component-1/src/app/quiz/quiz.component.ts
+++ implement-a-reusable-dialog-component-1/src/app/quiz/quiz.component.ts
@@ -1,31 +1,30 @@
-import {Component, inject, input} from '@angular/core';
-import {Question} from '../data.models';
-import {QuizService} from '../quiz.service';
-import {Router} from '@angular/router';
+import { Component, inject, input } from '@angular/core';
+import { Question } from '../data.models';
+import { QuizService } from '../quiz.service';
+import { Router } from '@angular/router';
 
 import { QuestionComponent } from '../question/question.component';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
-    selector: 'app-quiz',
-    templateUrl: './quiz.component.html',
-    styleUrls: ['./quiz.component.css'],
-    imports: [QuestionComponent]
+  selector: 'app-quiz',
+  templateUrl: './quiz.component.html',
+  styleUrls: ['./quiz.component.css'],
+  imports: [QuestionComponent, DialogComponent],
 })
 export class QuizComponent {
-
   questions = input<Question[]>([]);
 
-  userAnswers: string[] = new Array(5).fill("");
+  userAnswers: string[] = new Array(5).fill('');
   quizService = inject(QuizService);
   router = inject(Router);
 
   submit(): void {
     this.quizService.computeScore(this.questions(), this.userAnswers);
-    this.router.navigateByUrl("/result");
+    this.router.navigateByUrl('/result');
   }
 
   allAnswered(): boolean {
-    return this.userAnswers.every(ans => ans != "");
+    return this.userAnswers.every((ans) => ans != '');
   }
-
 }
