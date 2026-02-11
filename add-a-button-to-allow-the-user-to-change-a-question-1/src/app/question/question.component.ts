ec22182efffa24e13a97861283f5a06b60208e0d
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2026-02-11 19:28:17 +0200

    feat: enhance quiz functionality with question switching and UI improvements

--- add-a-button-to-allow-the-user-to-change-a-question-1/src/app/question/question.component.ts
+++ add-a-button-to-allow-the-user-to-change-a-question-1/src/app/question/question.component.ts
@@ -1,35 +1,34 @@
-import {Component, input, output} from '@angular/core';
-import {Question} from '../data.models';
-
+import { Component, input, output } from '@angular/core';
+import { Question } from '../data.models';
 
 @Component({
-    selector: 'app-question',
-    templateUrl: './question.component.html',
-    styleUrls: ['./question.component.css'],
-    imports: []
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css'],
+  imports: [],
 })
 export class QuestionComponent {
-
-  question = input.required<Question>()
+  question = input.required<Question>();
   correctAnswer = input<string>();
   userAnswer = input<string>();
+  canSwitch = input<boolean>(true);
   change = output<string>();
+  switch = output<Question>();
   currentSelection!: string;
 
-
   getButtonClass(answer: string): string {
-    if (! this.userAnswer()) {
-        if (this.currentSelection == answer)
-          return "tertiary";
+    if (!this.userAnswer()) {
+      if (this.currentSelection == answer) return 'tertiary';
     } else {
-      if (this.userAnswer() == this.correctAnswer() && this.userAnswer() == answer)
-        return "tertiary";
-      if (answer == this.correctAnswer())
-        return "secondary";
+      if (
+        this.userAnswer() == this.correctAnswer() &&
+        this.userAnswer() == answer
+      )
+        return 'tertiary';
+      if (answer == this.correctAnswer()) return 'secondary';
     }
-    return "primary";
+    return 'primary';
   }
-
 
   buttonClicked(answer: string): void {
     this.currentSelection = answer;
--- add-a-button-to-allow-the-user-to-change-a-question-1/src/app/quiz.service.ts
+++ add-a-button-to-allow-the-user-to-change-a-question-1/src/app/quiz.service.ts
@@ -1,46 +1,91 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {map, Observable, tap} from 'rxjs';
-import {Category, Difficulty, ApiQuestion, Question, Results, SubCategory} from './data.models';
+import { Injectable, Signal, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, tap } from 'rxjs';
+import {
+  Category,
+  Difficulty,
+  ApiQuestion,
+  Question,
+  Results,
+  SubCategory,
+} from './data.models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuizService {
-
-  private API_URL = "https://opentdb.com/";
+  private API_URL = 'https://opentdb.com/';
   private latestResults!: Results;
+  private currentQuestions = signal<Question[]>([]);
+  private canSwitch = signal(true);
+  readonly canSwitchQuestion = this.canSwitch.asReadonly();
+  private catId = signal<string>('');
+  private difficulty = signal<Difficulty>('Easy');
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getAllCategories(): Observable<Category[]> {
-    return this.http.get<{ trivia_categories: Category[] }>(this.API_URL + "api_category.php").pipe(
-      map(res => res.trivia_categories),
-      map(categories => this.populateSubCategories(categories)),
-    );
+    return this.http
+      .get<{ trivia_categories: Category[] }>(this.API_URL + 'api_category.php')
+      .pipe(
+        map((res) => res.trivia_categories),
+        map((categories) => this.populateSubCategories(categories)),
+      );
   }
 
-  createQuiz(categoryId: string | number, difficulty: Difficulty): Observable<Question[]> {
-    return this.http.get<{ results: ApiQuestion[] }>(
-        `${this.API_URL}/api.php?amount=5&category=${categoryId}&difficulty=${difficulty.toLowerCase()}&type=multiple`)
+  createQuiz(
+    categoryId: string | number,
+    difficulty: Difficulty,
+  ): Signal<Question[]> {
+    this.catId.set(categoryId.toString());
+    this.difficulty.set(difficulty);
+    this.getQuestions(5)
+      .pipe(tap((questions) => this.currentQuestions.set(questions)))
+      .subscribe();
+    this.canSwitch.set(true);
+    return this.currentQuestions.asReadonly();
+  }
+
+  switchQuestion(question: Question): Signal<Question[]> {
+    this.getQuestions(1).subscribe((newQuestions) => {
+      let index = this.currentQuestions().findIndex(
+        (q) => q.question === question.question,
+      );
+      if (index > -1) {
+        this.currentQuestions.update((questions) => {
+          questions.splice(index, 1, newQuestions[0]);
+          return [...questions];
+        });
+      }
+    });
+    this.canSwitch.set(false);
+    return this.currentQuestions.asReadonly();
+  }
+
+  private getQuestions(count = 5): Observable<Question[]> {
+    return this.http
+      .get<{
+        results: ApiQuestion[];
+      }>(`${this.API_URL}/api.php?amount=${count}&category=${this.catId()}&difficulty=${this.difficulty().toLowerCase()}&type=multiple`)
       .pipe(
-        map(res => {
-          const quiz: Question[] = res.results.map(q => (
-            {...q, all_answers: [...q.incorrect_answers, q.correct_answer].sort(() => (Math.random() > 0.5) ? 1 : -1)}
-          ));
+        map((res) => {
+          const quiz: Question[] = res.results.map((q) => ({
+            ...q,
+            all_answers: [...q.incorrect_answers, q.correct_answer].sort(() =>
+              Math.random() > 0.5 ? 1 : -1,
+            ),
+          }));
           return quiz;
-        })
+        }),
       );
   }
 
   computeScore(questions: Question[], answers: string[]): void {
     let score = 0;
     questions.forEach((q, index) => {
-      if (q.correct_answer == answers[index])
-        score++;
-    })
-    this.latestResults = {questions, answers, score};
+      if (q.correct_answer == answers[index]) score++;
+    });
+    this.latestResults = { questions, answers, score };
   }
 
   getLatestResults(): Results {
@@ -52,26 +97,30 @@ export class QuizService {
     const catsWithSubCatsMap = new Map<string, SubCategory[]>();
     const catsWithSubCats = new Set<string>();
     // First, we go through all categories and find the ones with sub-categories
-    categories.forEach(category => {
+    categories.forEach((category) => {
       category.subCategories = [];
-      let split = category.name.split(":");
+      let split = category.name.split(':');
       if (split.length > 1) {
-        const categoryName =  split[0].trim();
-        const subCategoriesForCategory = catsWithSubCatsMap.get(categoryName) ?? [];
-        subCategoriesForCategory.push({ id: category.id, name: split[1].trim() });
+        const categoryName = split[0].trim();
+        const subCategoriesForCategory =
+          catsWithSubCatsMap.get(categoryName) ?? [];
+        subCategoriesForCategory.push({
+          id: category.id,
+          name: split[1].trim(),
+        });
         catsWithSubCatsMap.set(categoryName, subCategoriesForCategory);
         catsWithSubCats.add(split[0].trim());
       }
     });
     // Then we remove all main categories that have sub-categories
-    const basicCategories = categories.filter(cat => !cat.name.includes(":"));
+    const basicCategories = categories.filter((cat) => !cat.name.includes(':'));
     // We create a new array of categories with sub-categories
     catsWithSubCatsMap.forEach((subCategoriesForCategory, catName) => {
       categoriesWithSubCategories.push({
-          id: 0,
-          name: catName,
-          subCategories: subCategoriesForCategory,
-      })
+        id: 0,
+        name: catName,
+        subCategories: subCategoriesForCategory,
+      });
     });
     return [...categoriesWithSubCategories, ...basicCategories];
   }
