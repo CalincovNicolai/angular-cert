94c1f8e07534eed39f4f2d8f22d1de72e9ce0a85
Calincov Nicolai <77538373+CalincovNicolai@users.noreply.github.com>
2026-02-11 17:50:08 +0200

    feat: implement ratings component with two-way data binding and dynamic star display

--- add-ratings-component-as-a-custom-form-element-1/src/app/movie-details/my-movie-data/my-movie-data.component.ts
+++ add-ratings-component-as-a-custom-form-element-1/src/app/movie-details/my-movie-data/my-movie-data.component.ts
@@ -1,19 +1,28 @@
-import {Component, inject, input} from '@angular/core';
-import {RatingsComponent} from '../ratings/ratings.component';
-import {MovieDetails} from '../../model/movie.model';
-import {MoviesService} from '../../services/movies.service';
+import {
+  Component,
+  inject,
+  input,
+  OnInit,
+  WritableSignal,
+} from '@angular/core';
+import { RatingsComponent } from '../ratings/ratings.component';
+import { FormsModule } from '@angular/forms';
+import { MovieDetails } from '../../model/movie.model';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-my-movie-data',
-  imports: [
-    RatingsComponent
-  ],
+  imports: [RatingsComponent, FormsModule],
   templateUrl: './my-movie-data.component.html',
-  styleUrl: './my-movie-data.component.scss'
+  styleUrl: './my-movie-data.component.scss',
 })
-export default class MyMovieDataComponent {
-
+export default class MyMovieDataComponent implements OnInit {
   protected movie = input.required<MovieDetails>();
+
+  rating: WritableSignal<number>;
   private service = inject(MoviesService);
 
+  ngOnInit() {
+    this.rating = this.service.getRating(this.movie());
+  }
 }
